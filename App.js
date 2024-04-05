11625bc58825bb40940e62b7c90ee743e8871917
Aref Orumiehei <areforumiehei@gmail.com>
2024-04-05 15:51:10 +0330

    💄font: add digital and ubuntu fonts

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Calculator from './app/components/Calculator';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Calculator from "./app/pages/Calculator";
+import { useFonts } from "expo-font";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Calculator/>
-    </View>
-  );
+  const [fontsLoaded] = useFonts({
+    digital: require("./assets/fonts/digital/digital-7.ttf"),
+    digitalMono: require("./assets/fonts/digital/digital-7 (mono).ttf"),
+    digitalMonoItalic: require("./assets/fonts/digital/digital-7 (mono italic).ttf"),
+    digitalItalic: require("./assets/fonts/digital/digital-7 (italic).ttf"),
+    ubuntuR: require("./assets/fonts/ubuntu/Ubuntu-Regular.ttf"),
+    ubuntuM: require("./assets/fonts/ubuntu/Ubuntu-Medium.ttf"),
+    ubuntuB: require("./assets/fonts/ubuntu/Ubuntu-Bold.ttf"),
+    ubuntuL: require("./assets/fonts/ubuntu/Ubuntu-Light.ttf"),
+  });
+
+  if (fontsLoaded) {
+    return (
+      <View style={styles.container}>
+        <Calculator />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'blue',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
